9cce20b8d0838a7d247b457321dd2b3ead6e5105
Kevin <kevinpanacci@hotmail.it>
2020-02-29 14:17:37 +0100

    Piccolo problema fixato, ma se clicco su a si aprono tutti

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 $('.menu-item').click(function(){
     if ($(this).children('div').is(':visible')){
-        $(this).children('div').slideToggle(50);
+        $(this).children('div').slideToggle(200);
     } else {
-        $('.menu-item').children('div').slideToggle(50);
-        $(this).children('div').slideToggle(50);
+        $('.menu-item').children('div').slideUp(200);
+        $(this).children('div').slideToggle(200);
     }
 });
-
-// $('.menu-item').click(function() {
-//     $(this).children('div').slideToggle(50);
-// });
